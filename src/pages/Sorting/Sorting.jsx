a15035a6b630176f8855e878aba692687a4f27f1
jigyasajamra <jigyasajamra2023@gmail.com>
2025-10-11 19:51:19 +0530

    Updated the issue 64

--- src/pages/Sorting/Sorting.jsx
+++ src/pages/Sorting/Sorting.jsx
@@ -211,7 +211,7 @@ const AlgorithmList = ({ navigate }) => {
                   >
                     <div className="flex items-center gap-1">
                       <span className="text-xs font-medium text-gray-400">
-                        Visualize
+                        Solve
                       </span>
                       <ArrowLeft className="h-4 w-4 text-gray-400 rotate-180" />
                     </div>
